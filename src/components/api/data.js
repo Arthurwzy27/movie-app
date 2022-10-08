46fa302fe2ef697301e6aafc2fef9ce0edbc2c3b
Arthur Picard <arthur.picard7@gmail.com>
2022-10-08 15:31:52 +0200

    fake data created

---
+++ src/components/api/data.js
@@ -0,0 +1,6 @@
+export const datas = [
+  { title: 'Une bière au front', img: 'movie-img', year: 2022 },
+  { title: 'Au front', img: 'movie-img', year: 2018 },
+  { title: 'Bière  front', img: 'movie-img', year: 2002 },
+  { title: 'Frontière au front', img: 'movie-img', year: 2021 },
+]
