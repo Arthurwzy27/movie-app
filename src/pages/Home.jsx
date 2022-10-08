aef0039ec4f04a5bcf7c8ad87b5627f58c697585
Arthur Picard <arthur.picard7@gmail.com>
2022-10-08 14:55:27 +0200

    Different components created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Banner from '../components/Banner';
+import MovieCard from '../components/MovieCard';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <MovieCard />
+
+    </div>
+  )
+}
+
+export default Home
