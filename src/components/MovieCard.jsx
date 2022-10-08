f1ccc3ea634f30bf495b89743ac082b58b79c7f9
Arthur Picard <arthur.picard7@gmail.com>
2022-10-08 15:40:19 +0200

    Map over datas and start css movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { datas } from './api/data'
+
+import './scss/MovieCard.css';
 
 const MovieCard = () => {
   return (
-    <div>MovieCard</div>
+    <>
+    <div className='movie-card'>
+      <div className="movie-category">Popular</div>
+      <div className="movie-container">
+        {datas.map((movie, id) => {
+          return (
+            <div className='movieMap '>
+              <div className="movie-img"><img src={movie.img} alt='movie-img'/></div>
+              <div className="movie-infos">
+                <div className="movie-title">{movie.title}</div>
+                <div className="movie-year">{movie.year}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+    </>
   )
 }
 
