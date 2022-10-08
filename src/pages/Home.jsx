a944477d6b178350520f9871b23826a4b7baeac0
Arthur Picard <arthur.picard7@gmail.com>
2022-10-08 15:31:40 +0200

    Home css created and added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Navbar from '../components/Navbar';
 import Banner from '../components/Banner';
 import MovieCard from '../components/MovieCard';
 
+import './Home.css';
+
 const Home = () => {
   return (
     <div>
